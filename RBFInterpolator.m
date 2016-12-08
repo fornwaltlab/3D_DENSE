ee755e4f7282117a4066f6047ca65b2fc51d36b4
Jonathan Suever <suever@gmail.com>
2016-12-08 16:24:12 -0500

    Updates documentation and default Constant parameter

--- RBFInterpolator.m
+++ RBFInterpolator.m
@@ -1,15 +1,51 @@
 classdef RBFInterpolator < hgsetget
 % RBFInterpolator - Interpolator that uses radial basis functions
 %
-%   The RBFInterpolator class allows the user to easily compute radial basis
-%   functions for the supplied data. This class is extrenemely flexible and can
-%   be used for N-D data and can simultaneously interpolate several data
-%   associated with the specified points
+%   The RBFInterpolator class allows the user to easily compute radial
+%   basis functions for the supplied data. This class is extrenemely
+%   flexible and can be used for N-D data and can simultaneously
+%   interpolate several data associated with the specified points
+%
+%   You can create an interpolator from a series of points and values:
+%
+%   DEMO:
+%       % Create some x y points and their corresponding z values
+%       [x, y] = meshgrid(-10:10, -10:10);
+%       z = x.^2 + y.^2;
+%
+%       % Construct the interpolator
+%       RBF = RBFInterpolator([x(:), y(:)], z(:));
+%
+%       % Interpolate the value at unknown points
+%       [xq, yq] = meshgrid(linspace(-10, 10), linspace(-10, 10));
+%
+%       value = RBF(xq, yq);
+%
+%       % Now plot the results to show that the interpolation is decent
+%       plot3(xq(:), yq(:), value(:), '.')
+%
+% USAGE:
+%   R = RBFInterpolator(points, values, options)
+%
+% INPUTS:
+%   points:     [M x N] Array, An N-dimensional set of points at which the
+%               values are defined.
+%
+%   values:     [M x 1] Array, Values corresponding to each row in the
+%               POINTS input matrix.
+%
+%   options:    Parameter/Value Pairs, Other properties of the
+%               RBFInterpolator object that should be specified as
+%               parameter and value pairs (optional)
+%
+% OUTPUTS:
+%   R:      Object, Handle to the RBFInterpolator object which can be used
+%           to query the interpolator at a specific point.
 %
 % Last Modified: 10-14-2013
 % Modified By: Jonathan Suever (suever@gmail.com)
 
-    properties
+    properties (SetAccess = 'protected')
         Constant            % Constant to be used for some basis functions
         Points      = []    % Locations (coordinates) of corresponding values
         Data        = []    % Values associated with each location
@@ -90,6 +126,9 @@ classdef RBFInterpolator < hgsetget
             %
             %                       @(radius, constant)function()
             %
+            %     Normalized:   Determines whether to use normalized RBFs
+            %                   (true) or not (false, the default).
+            %
             % OUTPUTS:
             %   R:      RBFInterpolator, object handle to be used to
             %           perform interpolation.
@@ -97,16 +136,20 @@ classdef RBFInterpolator < hgsetget
             self.Points = pts;
             self.Data = vals;
 
+            % Make sure that the data format is correct
             assert(size(self.Points, 1) == size(self.Data, 1),...
                 'Data must be the same size as your points');
 
+            % Process all of the input arguments and assign the necessary
+            % properties
             set(self, varargin{:});
 
             % Compute the average distance between nodes
             if isempty(self.Constant)
-                D = max(self.Points) - min(self.Points);
-                self.Constant = (prod(D) / self.NPoints)^ ...
-                                (1 / size(self.Points, 2));
+                npts = size(pts, 1);
+                tmp = bsxfun(@minus, permute(pts, [3 2 1]), pts);
+                distances = reshape(sqrt(sum(tmp.^2, 2)), npts, npts);
+                self.Constant = mean(distances(:));
             end
 
             % Go ahead and solve according to our data
@@ -189,8 +232,8 @@ classdef RBFInterpolator < hgsetget
         function B = subsref(self, S)
             % subsref - Overloaded subscript referencing
             %
-            %   This is overloaded because we want to be able to use indices to
-            %   interpolate the RBF
+            %   This is overloaded because we want to be able to use
+            %   indices to interpolate the RBF
 
             switch S(1).type
                 case '()'
@@ -199,7 +242,10 @@ classdef RBFInterpolator < hgsetget
                     B = builtin('subsref', self, S);
             end
         end
+    end
 
+    %-- Get / Set Methods --%
+    methods
         function res = get.NPoints(self)
             res = size(self.Points, 1);
         end
